46327755744013722cc7024b10aaf8e8db346d01
azazack <zakaria.azf@gmail.com>
2020-01-10 12:15:49 +0100

    Add Pug and Login

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.login-page
+
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  name: "Login"
+})
+export default class Login extends Vue {}
+</script>
+<style lang="scss">
+.login-page {
+  height: 100%;
+  background-color: aqua;
+}
+</style>
