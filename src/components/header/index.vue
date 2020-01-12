9a26ce48d3a8639229553ef952bf7c5c7dc5c25d
metanas <com.com404@gmail.com>
2020-01-12 15:50:16 +0100

    Add header

---
+++ src/components/header/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.container-fluid.header-content
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "PublicHeader"
+})
+export default class PublicHeader extends Vue {}
+</script>
+
+<style lang="scss" src="../../stylesheets/header/index.scss"/>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,22 @@
 <template lang="pug">
-.login-page.d-flex.align-items-center
-  .container
-    .row
-      .col-md-6.offset-md-3.login-card
-        input-attr(name='input' :value='value' label="Email" placeholder="email@email.com")
-        input-attr(name='input' :value='value' type="password" label="Password" placeholder="*********")
-        button.btn.btn-minizen.float-right(type='submit') connectez vous
-
+.container-fluid.vh-100
+  .row
+    PublicHeader
+  .login-page.d-flex.align-items-center
+    .container
+      .row
+        .col-md-6.offset-md-3.login-card
+          input-attr(name='input' :value='value' label="Email" placeholder="email@email.com")
+          input-attr(name='input' :value='value' type="password" label="Password" placeholder="*********")
+          button.btn.btn-minizen.float-right(type='submit') connectez vous
 </template>
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import inputAttr from "@/components/inputs/inputAttr.vue";
+import PublicHeader from "@/components/header/index.vue";
 @Component({
   name: "Login",
-  components: { inputAttr }
+  components: { inputAttr, PublicHeader }
 })
 export default class Login extends Vue {
   value = "";
@@ -25,17 +28,18 @@ export default class Login extends Vue {
 @import "~bootstrap/scss/utilities/display";
 @import "~bootstrap/scss/buttons";
 @import "~bootstrap/scss/utilities/float";
+@import "~bootstrap/scss/utilities/sizing";
 
 .login-page {
-  height: 100%;
+  height: calc(100% - 70px);
+  width: 100%;
 
-
-  .login-card{
+  .login-card {
     background-color: #2d2c2c;
     padding: 30px;
     border-radius: 14px;
 
-    label{
+    label {
       color: white;
     }
   }
